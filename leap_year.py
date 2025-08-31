0c6f113b40ae1c4a28e201c8f770c86b2b8314ff
Diego Baldassare <diegobaldassare@gmail.com>
2025-08-31 20:37:21 -0300

    Complete practice

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,9 @@
 def leap_year():
-    print("TO DO")
+    year = int(input("Ingrese un año: "))
+
+    isLeapYear = year % 4 == 0 or (year % 100 == 0 and year % 400 == 0)
+
+    if (isLeapYear):
+        print(f"El año {year} es bisiesto")
+    else:
+        print(f"El año {year} no es bisiesto")
--- line.py
+++ line.py
@@ -1,2 +1,23 @@
+import math
+
 def line():
-    print("TO DO")
+    a = float(input("Ingrese el coeficiente A: "))
+    b = float(input("Ingrese el coeficiente B: "))
+    x1 = float(input("Ingrese el coeficiente X1: "))
+    x2 = float(input("Ingrese el coeficiente X2: "))
+    y1 = a * x1 + b
+    y2 = a * x2 + b
+
+    print(f"El coeficiente A de su ecuación de la recta es: {a}")
+    print(f"El coeficiente B de su ecuación de la recta es: {b}")
+    print(f"El coeficiente X1 de su ecuación de la recta es: {x1}")
+    print(f"El coeficiente X2 de su ecuación de la recta es: {x2}")
+    print(f"\nPara la siguiente ecuación:\nY = {a}X + {b}\n")
+    print(f"Dados los siguientes puntos:\n\tP1 ({x1}, {y1})\n\tP2 ({x2}, {y2})\n")
+
+    # https://es.wikipedia.org/wiki/Distancia#Distancia_de_dos_puntos_en_el_plano
+    # d(ab) = sqrt((x2 - x1)^2 + (y2 - y1)^2)
+
+    # https://www.w3schools.com/python/module_math.asp
+    # math.pow() math.sqrt()
+    print(f"La distancia entre ellos es: {math.sqrt(math.pow((x2 - x1), 2) + math.pow((y2 - y1), 2))}")
